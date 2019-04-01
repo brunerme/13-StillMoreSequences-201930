f0a6e683ec69695c0cee0fabe7234939f059747a
brunerme <brunerme@rose-hulman.edu>
2019-04-01 13:32:20 -0400

    done.

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -11,8 +11,8 @@ for ITERATING through SEQUENCES, including selections from:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Maria Bruner.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -104,9 +104,15 @@ def shortest_string(strings):
       :type strings: list[str]   or tuple(str)
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    index_of_min = 0
+    for k in range(1, len(strings)):
+        if len(strings[k]) < len(strings[index_of_min]):
+            index_of_min = k
+    return strings[index_of_min]
+
 
 
 def run_test_index_of_largest_number():
@@ -181,9 +187,14 @@ def index_of_largest_number(numbers, n):
 
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    index_of_max = 0
+    for k in range(1, n):
+        if numbers[k] > numbers[index_of_max]:
+            index_of_max = k
+    return index_of_max
 
 
 # -----------------------------------------------------------------------------
@@ -236,9 +247,15 @@ def number_of_stutters(s):
        :type s: str
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    count = 0
+    for k in range(len(s)-1):
+        if s[k+1] == s[k]:
+            count = count + 1
+    return count
+
 
 
 def run_test_is_palindrome():
@@ -309,7 +326,7 @@ def is_palindrome(s):
       :type s: str
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ###########################################################################
@@ -318,8 +335,17 @@ def is_palindrome(s):
     #   above are particularly good examples to work by hand.
     ###########################################################################
     # -------------------------------------------------------------------------
+    sequence = []
+    for k in range(len(s)//2):
+        if s[k] == s[len(s)-1-k]:
+            sequence = sequence + [1]
+        else:
+            sequence = sequence + [0]
 
-
+    if min(sequence) == 0:
+        return False
+    else:
+        return True
 # -----------------------------------------------------------------------------
 # Some problems loop (iterate) through two or more sequences
 #    IN PARALLEL, as in the   count_same   problem below.
@@ -376,10 +402,14 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # -------------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    count = 0
+    for k in range(len(sequence1)):
+        if sequence1[k] == sequence2[k]:
+            count = count+1
+    return count
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
